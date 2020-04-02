ee4eb670a5cda218e3a5cb3dcb50501127e56105
Jaycee Chow <2832203391@qq.com>
2020-04-03 01:06:18 +0800

    update

---
+++ build.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/lifeInZUCC/lifeInZUCC.github.io ../lifeInZUCC.github.io --depth=1
+mkdocs build --clean
+cd ./lifeInZUCC.github.io
+git add *
+git commit -m "update"
+git push origin master
